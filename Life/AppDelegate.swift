7c1f67fa9eec9f5dc1e9551ed1871a366347db19
Joe Wilcoxson <joe.wilcoxson@gmail.com>
2016-04-12 19:47:26 -0400

    Initial Commit

---
+++ Life/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Life
+//
+//  Created by Joe Wilcoxson on 4/12/16.
+//  Copyright © 2016 Joe Wilcoxson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
